b2ac4897032655965bfbace56d281e09c2685bdc
Jedi_Supreme <jediassisi@gmail.com>
2019-04-02 10:37:03 +0000

    Initial commit

---
+++ app/src/main/java/com/jedit/kenklinadmin/activities/SetupActivity.java
@@ -0,0 +1,15 @@
+package com.jedit.kenklinadmin.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jedit.kenklinadmin.R;
+
+public class SetupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup);
+    }
+}
